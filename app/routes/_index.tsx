bd10754efef6ab5f987eb10f698b0ad767b1abf4
Alan Lee <lky0201@hotmail.com>
2024-02-11 15:49:53 -0600

    Update _index.tsx
    Update meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => {
   return [
     { title: "Alan Lee | Terminal" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to my profile!" },
   ];
 };
 
